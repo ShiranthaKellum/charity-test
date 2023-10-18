eeac2b827797308a87d4dcc6c87aa7271e0c457c
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2023-10-18 12:06:39 +0530

    User registration.

--- src/main/java/com/bezkoder/spring/security/mongodb/models/Patient.java
+++ src/main/java/com/bezkoder/spring/security/mongodb/models/Patient.java
@@ -16,17 +16,17 @@ public class Patient {
     @Id
     private String id;
     private String name;
-    private String age;
+//    private String age;
     private String gender;
-    private String country;
-    private String height;
-    private String weight;
-    private String heartRate;
-    private String bloodPressure;
-    private String sugarLevel;
-    private List<String> allergies;
-    private List<String> symptoms;
-    private List<String> diseases;
-    private List<String> treatments;
-    private List<String> medicines;
+//    private String country;
+//    private String height;
+//    private String weight;
+//    private String heartRate;
+//    private String bloodPressure;
+//    private String sugarLevel;
+    private String allergies;
+//    private String symptoms;
+//    private String diseases;
+//    private String treatments;
+//    private String medicines;
 }
