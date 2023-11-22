e5a9a7bc066440fea992df6c17a38118049cd789
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2023-11-22 08:36:59 +0530

    Admin role.

--- src/main/java/com/bezkoder/spring/security/mongodb/models/ERole.java
+++ src/main/java/com/bezkoder/spring/security/mongodb/models/ERole.java
@@ -1,9 +1,9 @@
 package com.bezkoder.spring.security.mongodb.models;
 
 public enum ERole {
+  ROLE_ADMIN,
   ROLE_DOCTOR,
   ROLE_PATIENT,
   ROLE_CONTRIBUTOR,
-  ROLE_USER,
-  ROLE_ADMIN
+  ROLE_USER
 }
